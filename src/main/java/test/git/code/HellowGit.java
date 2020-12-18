f782990c53121c73a6d8c2e421ecef9c81233e83
liuxiaoq1 <liuxiaoq1@yonyou.com>
2020-12-18 15:37:46 +0800

    first commit!

---
+++ src/main/java/test/git/code/HellowGit.java
@@ -0,0 +1,11 @@
+package test.git.code;
+
+/**
+ * @author liuxiaoq1
+ * @date 2020/12/18 - 15:30
+ **/
+public class HellowGit {
+    public static void main(String[] args) {
+        System.out.println("HellowGit!");
+    }
+}
